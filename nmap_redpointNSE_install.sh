ed89a09cebb53ef156871c899ef3e85a9469e999
qt-pie <48164033+qt-pie@users.noreply.github.com>
2020-10-23 23:11:09 -0700

    Delete nmap_redpointNSE_install.sh

--- nmap_redpointNSE_install.sh
+++
@@ -1,5 +0,0 @@
-# NOTE - keep this cheatsheet handy - https://github.com/gnebbia/nmap_tutorial/blob/master/sections/ics_scada.md
-mkdir -p $HOME/tools/scada
-git clone https://github.com/digitalbond/Redpoint $HOME/tools/scada/Redpoint
-cp $HOME/tools/scada/Redpoint/*.nse /usr/share/nmap/scripts/
-sudo nmap --script-updatedb
